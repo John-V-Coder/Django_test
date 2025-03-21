935a981c2c7bb0566cb238add3050a9f09bc691b
John_V_Coder <omondijohn669@gmail.com>
2025-03-21 11:54:40 +0300

    home template test

--- base/views.py
+++ base/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name':'Lets learn Python'},
+    {'id': 2, 'name':'Django is awesome'},
+    {'id': 3, 'name':'full stack development'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
