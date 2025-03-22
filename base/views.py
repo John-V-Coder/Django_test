bde96f5caeff4c7e1dc9a1a993f658ca54029d09
John_V_Coder <omondijohn669@gmail.com>
2025-03-22 11:36:16 +0300

    dynamic url

--- base/views.py
+++ base/views.py
@@ -4,13 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 rooms = [
-    {'id': 1, 'name':'Lets learn Python'},
-    {'id': 2, 'name':'Django is awesome'},
-    {'id': 3, 'name':'full stack development'},
+    {'id': 1, 'name': 'Lets learn Python!'},
+    {'id': 2, 'name': 'Django is awesome'},
+    {'id': 3, 'name': 'full stack development'},
 ]
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    return render(request, 'base/room.html')
